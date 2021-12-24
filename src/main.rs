1a5d7f4b18ab9cb5fc3cf5c39ee417b25a2ace7c
JustAGod <hvalick2111@gmail.com>
2021-12-24 22:50:26 +0300

    kek

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn plus(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::plus;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, plus(a, b));
+    }
+}
